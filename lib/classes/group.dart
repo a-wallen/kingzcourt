94e871306a036cc9101637be61a92992a94d8d16
David-S-2002 <dstanko@seattleu.edu>
2020-12-01 11:13:04 -0800

    Getters and setters for classes
    I wrote some getters and setters for the Player and Group classes.

--- lib/classes/group.dart
+++ lib/classes/group.dart
@@ -1,14 +1,40 @@
 import 'player.dart';
-class Group{
+
+class Group {
   int groupSize; //number of people in a group
   String groupName;
-  List playerList[] = new Player[groupSize]; //array of Players
+  List<Player> playerList; //array of Players
 
-  Group(int myGroupSize, String myGroupName, Player[] myPlayerList){
+  Group(int myGroupSize, String myGroupName, List<Player> myPlayerList) {
     groupSize = myGroupSize;
     groupName = myGroupName;
-    
-    for(int i = 0; i < myPlayerList.size(); i++)
+
+    for (int i = 0; i < myPlayerList.length; i++)
       playerList[i] = myPlayerList[i];
+  }
+
+  //getters:
+  int getGroupSize() {
+    return groupSize;
+  }
+
+  String getGroupName() {
+    return groupName;
+  }
+
+  List<Player> getPlayerList() {
+    return playerList;
+  }
+
+  //setters:
+  //adds a player and sets their waiting time to 0
+  void addPlayer(int playersToAdd, String position, String imageFilePath) {
+    groupSize++;
+    playerList.add(Player(position, 0, imageFilePath));
+  }
+
+  //sets group name
+  void setGroupName(String newGroupName) {
+    groupName = newGroupName;
   }
 }
--- lib/classes/player.dart
+++ lib/classes/player.dart
@@ -1,12 +1,44 @@
 class Player {
   //String rank;
   String position;
-  int waitingTime;
+  int waitingTime; //measured by the number of games waited
   String imageFilePath; //file path to the profile picture
 
   Player(String myPosition, int myWaitingTime, String myImageFilePath) {
     position = myPosition;
     waitingTime = myWaitingTime;
     imageFilePath = myImageFilePath;
+  }
+
+  //getters:
+  String getPosition() {
+    return position;
+  }
+
+  int getWaitingTime() {
+    return waitingTime;
+  }
+
+  String getImageFilePath() {
+    return imageFilePath;
+  }
+
+  //setters:
+  void setPosition(String newPosition) {
+    position = newPosition;
+  }
+
+  //adds one more game to user's waiting time
+  void increaseWaitingTime() {
+    waitingTime++;
+  }
+
+  //sets the waiting time to -1 to indicate that the player wants to play ASAP
+  void playASAP() {
+    waitingTime = -1;
+  }
+
+  void setImageFilePath(String newImageFilePath) {
+    imageFilePath = newImageFilePath;
   }
 }
