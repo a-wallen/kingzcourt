6db51809e54d6d37d99145eb0f4d36cdc4286240
Stephen (Alex) Wallen <wallenstephe@seattleu.edu>
2020-12-02 16:23:49 -1000

    File Organization

---
+++ lib/database/initDatabase.dart
@@ -0,0 +1,28 @@
+import 'package:path_provider/path_provider.dart';
+
+import 'dart:io';
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart';
+
+class DatabaseHelper {
+  static final _dbName = "myDatabase.db";
+  static final _dbVersion = 1;
+  DatabaseHelper._privateConstructor();
+  static final DatabaseHelper instance = DatabaseHelper._privateConstructor();
+  static Database _database;
+
+  Future<Database> get database async {
+    if (_database != null) return _database;
+    _database = await _initiateDatabase();
+    return _database;
+  }
+
+  _initiateDatabase() async {
+    Directory dir = await getApplicationDocumentsDirectory();
+    String path = join(dir.path, _dbName);
+    return await openDatabase(path, version: _dbVersion, onCreate: _onCreate);
+  }
+
+  Future _onCreate(Database db, int version) {}
+}
