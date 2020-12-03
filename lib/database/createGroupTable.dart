4623df3235ea78d1326675961f08e726159047f3
Hector Herrada <hector23herrada@gmail.com>
2020-12-03 13:22:26 -0800

    changes to cREATE player and group entities

--- lib/database/createGroupTable.dart
+++ lib/database/createGroupTable.dart
@@ -2,9 +2,22 @@ import 'package:sqflite/sqflite.dart';
 
 Future<void> createGroupTable(Database db, int version) {
   db.execute('''
-    CREATE TABLE GROUP
-    GID INTEGER PRIMARY KEY,
-    SIZE INTEGER NOT NULL,
-    NAME TEXT NOT NULL,
+    CREATE TABLE [GROUP] (
+    g_id        INTEGER PRIMARY KEY
+                        NOT NULL
+                        UNIQUE,
+    num_players INTEGER NOT NULL,
+    group_name  TEXT    NOT NULL
+);
     ''');
 }
+
+/*
+CREATE TABLE [GROUP] (
+    g_id        INTEGER PRIMARY KEY
+                        NOT NULL
+                        UNIQUE,
+    num_players INTEGER NOT NULL,
+    group_name  TEXT    NOT NULL
+);
+*/
--- lib/database/createPlayerTable.dart
+++ lib/database/createPlayerTable.dart
@@ -1,13 +1,25 @@
 import 'package:sqflite/sqflite.dart';
 
 Future<void> createPlayerTable(Database db, int version) {
-  db.execute('''
-    CREATE TABLE IF NOT EXISTS 
-    PLAYER (
-    PID INTEGER PRIMARY KEY,
-    NAME TEXT NOT NULL,
-    POSITION TEXT NOT NULL,
-    IMAGE TEXT NOT NULL
-    )
-    ''');
+  db.execute('''CREATE TABLE PLAYER (
+    p_id        INTEGER PRIMARY KEY ASC AUTOINCREMENT
+                          UNIQUE
+                         NOT NULL,
+    first_name  TEXT NOT NULL,
+    last_name   TEXT NOT NULL,
+    position    TEXT NOT NULL,
+    profile_pic TEXT
+);''');
 }
+
+/*
+CREATE TABLE PLAYER (
+    p_id        INTEGER PRIMARY KEY ASC AUTOINCREMENT
+                          UNIQUE
+                         NOT NULL,
+    first_name  TEXT NOT NULL,
+    last_name   TEXT NOT NULL,
+    position    TEXT NOT NULL,
+    profile_pic TEXT
+);
+*/
