b82995f98e3ac3e1afc035e20336fbcd5b932ac2
David-S-2002 <dstanko@seattleu.edu>
2020-12-09 13:18:36 -0800

    Improved matching algo

---
+++ lib/algorithms/matching_algo_ihor.dart
@@ -0,0 +1,94 @@
+import 'package:kingzcourt/classes/Playlist.dart';
+import 'package:kingzcourt/classes/player.dart';
+import 'package:kingzcourt/classes/team_ihor.dart';
+
+//optional: 1 defense specialist
+//Input: Playlist
+//Output: 2 lists, one for each team
+
+void algorithm(Team team1, Team team2, Playlist playlist) {
+  Player currentPlayer;
+  Player firstPlayer; //to be matched and removed in while loop
+
+  //TODO Make check if playlist is less then teams size;
+  if (playlist.length < team1.teamSize + team2.teamSize) {
+    print('ALERT_' * 5);
+  }
+
+  int index;
+  int curNumberWaited;
+  int curCheckPlayer = playlist.first.getWaitingTime();
+
+  while (!team1.isTeamFull() || !team2.isTeamFull()) {
+    index = 0;
+    curNumberWaited = playlist.numberOfPlayersByWaitingTime(curCheckPlayer);
+
+    for (int i = 0; index < curNumberWaited; i++) {
+      currentPlayer = playlist.elementAt(index);
+      if (currentPlayer.getSkipGame() == false) {
+        if (i % 2 == 0) {
+          if (team1.isPositionAvailiable(currentPlayer.getPosition())) {
+            team1.addPlayer(currentPlayer);
+            team1.removePosition(currentPlayer.getPosition());
+            playlist.remove(currentPlayer);
+            curNumberWaited--;
+          } else if (team2.isPositionAvailiable(currentPlayer.getPosition())) {
+            team2.addPlayer(currentPlayer);
+            team2.removePosition(currentPlayer.getPosition());
+            playlist.remove(currentPlayer);
+            curNumberWaited--;
+          } else {
+            index++;
+          }
+        } else {
+          if (team2.isPositionAvailiable(currentPlayer.getPosition())) {
+            team2.addPlayer(currentPlayer);
+            team2.removePosition(currentPlayer.getPosition());
+            playlist.remove(currentPlayer);
+            curNumberWaited--;
+          } else if (team1.isPositionAvailiable(currentPlayer.getPosition())) {
+            team1.addPlayer(currentPlayer);
+            team1.removePosition(currentPlayer.getPosition());
+            playlist.remove(currentPlayer);
+            curNumberWaited--;
+          } else {
+            index++;
+          }
+        }
+      }
+    }
+​
+    while (
+        curNumberWaited != 0 && (!team1.isTeamFull() || !team2.isTeamFull())) {
+      firstPlayer = playlist.first;
+      if (team1.isTeamFull() == false) {
+        team1.addPlayer(firstPlayer);
+        team1.getPositions().remove(team1.getPositions().last);
+        playlist.remove(firstPlayer);
+      } else if (team2.isTeamFull() == false) {
+        team2.addPlayer(firstPlayer);
+        team2.getPositions().remove(team2.getPositions().last);
+        playlist.remove(firstPlayer);
+      }
+    }
+    curCheckPlayer--;
+  }
+​
+  // while ((!team1.isTeamFull() || !team2.isTeamFull()) && playlist.isNotEmpty) {
+  //   firstPlayer = playlist.first;
+  //   if (team1.isTeamFull() == false) {
+  //     team1.addPlayer(firstPlayer);
+  //     team1.getPositions().remove(team1.getPositions().last);
+  //     playlist.remove(firstPlayer);
+  //   } else if (team2.isTeamFull() == false) {
+  //     team2.addPlayer(firstPlayer);
+  //     team2.getPositions().remove(team2.getPositions().last);
+  //     playlist.remove(firstPlayer);
+  //   }
+  // }
+​
+  for (Player player in playlist) {
+    player.increaseWaitingTime();
+    player.changeSkipGame();
+  }
+}
---
+++ lib/classes/team_ihor.dart
@@ -0,0 +1,145 @@
+import 'package:kingzcourt/classes/player.dart';
+
+List<Object> teamOfTwo = [
+  ['OH', 'L'],
+  ['OP', 'M', 'S']
+];
+
+List<Object> teamOfThree = [
+  ['L', 'OH'],
+  'S',
+  ['OP', 'M']
+];
+
+List<Object> teamOfFour = [
+  'OH',
+  'OP',
+  'S',
+  ['M', 'L']
+];
+
+List<Object> teamOfFive = ['OH', 'OP', 'S', 'M', 'L'];​
+List<Object> teamOfSix = ['OH', 'OH', 'OP', 'S', 'M', 'L'];
+
+class Team {
+  String _teamName;
+  int _teamSize; //2-6 players
+  List<Player> _playerList; //list of Players on the Team
+  
+  List<Object> _positions;
+
+  Team(String myTeamName, int myTeamSize) {
+    _teamName = myTeamName;
+
+    updateTeamSize(myTeamSize);
+
+    _playerList = List(); //makes an empty list, leaving it empty for now
+  }
+
+  //getters:
+  String getTeamName() {
+    return _teamName;
+  }
+
+  List<Player> getPlayersOnTeam() {
+    return _playerList;
+  }
+
+  List<Object> getPositions() {
+    return _positions;
+  }
+
+  int get teamSize => _teamSize;
+
+  void removePosition(String position) {
+    if (_teamSize == 5 || _teamSize == 6) {
+      _positions.remove(position);
+      return;
+    } else {
+      for (Object o in _positions) {
+        if (o.runtimeType != String) {
+          if ((o as List<String>).contains(position)) {
+            _positions.remove(o);
+            return;
+          }
+        } else if ((o as String) == position) {
+          _positions.remove(o);
+          return;
+        }
+      }
+    }
+  }
+
+  //TODO implement contains positions
+  bool isPositionAvailiable(String position) {
+    bool isAvailiable = false;
+    if (_teamSize == 5 || _teamSize == 6) {
+      return _positions.contains(position);
+    } else {
+      for (Object o in _positions) {
+        if (o.runtimeType != String) {
+          if ((o as List<String>).contains(position)) {
+            isAvailiable = true;
+            return isAvailiable;
+          }
+        } else {
+          if ((o as String) == position) {
+            isAvailiable = true;
+            return isAvailiable;
+          }
+        }
+      }
+    }
+    return isAvailiable;
+  }
+
+  bool isTeamFull() {
+    return _positions.isEmpty;
+  }
+​
+  void updateTeamSize(int newTeamSize) {
+    _teamSize = newTeamSize;
+​
+    switch (newTeamSize) {
+      case 2:
+        _positions = teamOfTwo;
+        break;
+      case 3:
+        _positions = teamOfThree;
+        break;
+      case 4:
+        _positions = teamOfFour;
+        break;
+      case 5:
+        _positions = teamOfFive;
+        break;
+      case 6:
+        _positions = teamOfSix;
+        break;
+      default:
+        _positions = teamOfSix;
+        break;
+    }
+  }
+
+  //setters:
+  void setTeamName(String newTeamName) {
+    _teamName = newTeamName;
+  }
+
+  void addPlayer(Object o) {
+    Player player = o;
+    _playerList.add(player);
+  }
+
+  void removePlayer(Object o) {
+    Player playerToRemove = o;
+    _playerList.remove(playerToRemove);
+  }
+
+  //not sure how to return the player list so it can be printed
+  /*
+  String toString() {
+    return "Team name: $getTeamName() " + "Team size: $_playerList.size";
+  }*/
+}
