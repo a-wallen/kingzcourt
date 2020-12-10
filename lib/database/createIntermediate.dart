c874913887421e3d93b9bfce9978472eacfbed57
Hector Herrada <hector23herrada@gmail.com>
2020-12-10 12:43:25 -0800

    Updates to PLAYER, GROUP, INTERMEDIATE table
    INTERMEDIATE: added a new priamry key and kept the rest of the attribute as foreign keys
    
    REMOVE GROUP: added a remove query that removes all instances of the group (in INTERMEDIATE table )
    
    PLAYER: added a remove query that removes all instances of the player (in INTERMEDIATE table )

--- lib/database/createIntermediate.dart
+++ lib/database/createIntermediate.dart
@@ -3,23 +3,24 @@ import 'package:sqflite/sqflite.dart';
 Future<void> createIntermediateTable(Database db, int version) {
   db.execute('''
    CREATE TABLE PLAYER_GROUP (
-    p_id INTEGER PRIMARY KEY
-                 REFERENCES PLAYER (p_id) 
-                 NOT NULL,
-    g_id INTEGER PRIMARY KEY
-                 REFERENCES [GROUP] (g_id) 
-                 NOT NULL
+    g_id  INTEGER REFERENCES [GROUP] (g_id) 
+                  NOT NULL,
+    p_id  INTEGER REFERENCES PLAYER (p_id) 
+                  NOT NULL,
+    pg_id INTEGER PRIMARY KEY AUTOINCREMENT
+                  NOT NULL
     );
     ''');
 }
 
 /*
+pg_id is the arbitrary primary key for this intermediate table
 CREATE TABLE PLAYER_GROUP (
-    p_id INTEGER PRIMARY KEY
-                 REFERENCES PLAYER (p_id) 
-                 NOT NULL,
-    g_id INTEGER PRIMARY KEY
-                 REFERENCES [GROUP] (g_id) 
-                 NOT NULL
+    g_id  INTEGER REFERENCES [GROUP] (g_id) 
+                  NOT NULL,
+    p_id  INTEGER REFERENCES PLAYER (p_id) 
+                  NOT NULL,
+    pg_id INTEGER PRIMARY KEY AUTOINCREMENT
+                  NOT NULL
 );
 */
