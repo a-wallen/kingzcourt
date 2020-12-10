49f4383305c79b3cb7256ea31fbcd3e6df2d678c
Hector Herrada <hector23herrada@gmail.com>
2020-12-10 12:56:28 -0800

    Update to removeIntermediate.dart
    Using rawDelete() to delete an a row within INTERMEDIATE.

--- lib/database/removeIntermediate.dart
+++ lib/database/removeIntermediate.dart
@@ -1,6 +1,18 @@
 import 'package:sqflite/sqflite.dart';
 
 Future<int> removeIntermediateValue(Database db, int g_id, int p_id) async {
-  return await db.delete("INTERMEDIATE",
-      where: "g_id = ?, p_id = ?", whereArgs: [g_id, p_id]);
+  return await db.rawDelete('''
+delete from PLAYER_GROUP
+where p_id = ${p_id} 
+and g_id = ${g_id};''');
 }
+
+/*
+-- find where to delete in the intermediate
+g_id = ?, p_id = ?
+
+return await db.rawdelete('''
+delete from PLAYER_GROUP
+where p_id = ${p_id} 
+and g_id = ${g_id};''')
+*/
