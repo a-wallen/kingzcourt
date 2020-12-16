c83a8f5664a385ac352abff7590b00ba56d3adf4
varelandrew <61606497+varelandrew@users.noreply.github.com>
2020-12-15 19:40:42 -0800

    About Page Started, Images loaded in as Assets
    I started the about page where I have text in and the basic layout of how I want it to look, havn't been able to test it because of other errors. Then created an assets/images folder and added the images everyone sent me so then in pubspec.yaml I was able to create your asset images so I can call them in the about page so people can see what we look like :)

--- lib/pages/aboutinfo.dart
+++ lib/pages/aboutinfo.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:kingzcourt/classes/colors.dart';
 import 'package:kingzcourt/widgets/drawer.dart';
 
 class AboutPage extends StatefulWidget {
@@ -10,10 +10,137 @@ class AboutPage extends StatefulWidget {
 class _AboutPageState extends State<AboutPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        textTheme: Theme.of(context).textTheme,
-        title: Text("About"),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          textTheme: Theme.of(context).textTheme,
+          title: Text("About"),
+        ),
+        body: Row(
+          children: <Widget>[
+            Text(
+              "Our Mission",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 10.0,
+                color: AppColors.primaryDarkColor,
+              ),
+            ),
+            Text(
+              "We wanted to make an ease of access app that simulated an open-gym"
+              "scenario where teams would be picked based on our algorithm.",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 5.0,
+                color: AppColors.primaryDarkColor,
+              ),
+            ),
+            Text(
+              "Project Manager",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 10.0,
+                color: AppColors.primaryDarkColor,
+              ),
+            ),
+            Text(
+              "Alex Wallen",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 5.0,
+                color: AppColors.primaryDarkColor,
+              ),
+            ),
+            Text(
+              "Front-End",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 10.0,
+                color: AppColors.primaryDarkColor,
+              ),
+            ),
+            Text(
+              "Andrew Varela",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 5.0,
+                color: AppColors.primaryDarkColor,
+              ),
+            ),
+            Text(
+              "Katherine Outcalt",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 5.0,
+                color: AppColors.primaryDarkColor,
+              ),
+            ),
+            Text(
+              "Ezra Sackstein",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 5.0,
+                color: AppColors.primaryDarkColor,
+              ),
+            ),
+            Text(
+              "Back-End",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 10.0,
+                color: AppColors.primaryDarkColor,
+              ),
+            ),
+            Text(
+              "Bryce Chinn",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 5.0,
+                color: AppColors.primaryDarkColor,
+              ),
+            ),
+            Text(
+              "David Stanko",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 5.0,
+                color: AppColors.primaryDarkColor,
+              ),
+            ),
+            Text(
+              "Ihor Sherstiuk",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 5.0,
+                color: AppColors.primaryDarkColor,
+              ),
+            ),
+            Text(
+              "DataBases",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 10.0,
+                color: AppColors.primaryDarkColor,
+              ),
+            ),
+            Text(
+              "Alex Wallen",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 5.0,
+                color: AppColors.primaryDarkColor,
+              ),
+            ),
+            Text(
+              "Hector Herrada",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 5.0,
+                color: AppColors.primaryDarkColor,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
