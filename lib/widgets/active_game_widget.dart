14e27d6f570c9a08d91ffba0c77a5c443135d0ec
David-S-2002 <dstanko@seattleu.edu>
2020-12-16 15:02:10 -0800

    Fixed the state error (thanks, Bryce)!
    I tried making new teams when the refresh button is pressed, but I got an error "the getter teamSize was called on null".

--- lib/widgets/active_game_widget.dart
+++ lib/widgets/active_game_widget.dart
@@ -12,21 +12,29 @@ class ActiveGameWidget extends StatefulWidget {
   _ActiveGameWidgetState createState() => _ActiveGameWidgetState();
   Team _team1;
   Team _team2;
+  List<Team> _state = [Team('Team 1', 6), Team('Team 2', 6)];
 
   //constructor for teams that will be displayed:
   ActiveGameWidget(Team newTeam1, Team newTeam2) {
     _team1 = newTeam1;
     _team2 = newTeam2;
   }
+
+  makeNewGame(Team team1, Team team2, Playlist playlist) {
+    algorithm(team1, team2, playlist);
+    setState() {
+      _state = [team1, team2];
+    }
+
+    ;
+  }
 }
 
 class _ActiveGameWidgetState extends State<ActiveGameWidget> {
   @override
-  Team _team1 = Team('Team 1', 6);
-  Team _team2 = Team('Team 2', 6);
 
   //was trying to set the state by setting the teams that are playing, got some errors
-  /*var _state = [team1, team2];
+  /*List<Team> _state = [Team('Team 1', 6), Team('Team 2', 6)];
 
   makeNewGame(Team team1, Team team2, Playlist playlist) {
     algorithm(team1, team2, playlist);
@@ -40,7 +48,10 @@ class _ActiveGameWidgetState extends State<ActiveGameWidget> {
   Widget build(BuildContext context) {
     Team team1 = Team("Team 1", 6);
     Team team2 = Team("Team 2", 3);
+    int team1Size = team1.teamSize;
+    int team2Size = team2.teamSize;
     Player player = Player("Alex", "Wallen", "OH", 0, "path/path");
+
     return Stack(children: [
       //displaying the players on each team:
       //Text('Team 1'),
@@ -56,7 +67,7 @@ class _ActiveGameWidgetState extends State<ActiveGameWidget> {
               scrollDirection: Axis.vertical,
               children: [
                 //makes the number of active player widgets that correspond to the team size:
-                for (int i = 0; i < (team1.teamSize); i++) ActivePlayerWidget()
+                for (int i = 0; i < (team1Size); i++) ActivePlayerWidget()
               ])),
       //Positioning the other container to the right so that the two don't overlap:
       Positioned(
@@ -73,8 +84,7 @@ class _ActiveGameWidgetState extends State<ActiveGameWidget> {
                 scrollDirection: Axis.vertical,
                 children: [
                   //makes the number of active player widgets that correspond to the team size:
-                  for (int i = 0; i < (team2.teamSize); i++)
-                    ActivePlayerWidget()
+                  for (int i = 0; i < (team2Size); i++) ActivePlayerWidget()
                 ])),
       ),
 
