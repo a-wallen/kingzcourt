c405d5895a2ceb151bef813f452c717e17e19b1c
David-S-2002 <dstanko@seattleu.edu>
2020-12-16 13:57:35 -0800

    Worked on active game widget to display each team in a separate column

--- lib/widgets/active_game_widget.dart
+++ lib/widgets/active_game_widget.dart
@@ -10,21 +10,23 @@ import 'package:kingzcourt/algorithms/matching_algo_ihor.dart';
 class ActiveGameWidget extends StatefulWidget {
   @override
   _ActiveGameWidgetState createState() => _ActiveGameWidgetState();
-  Team team1;
-  Team team2;
+  Team _team1;
+  Team _team2;
 
   //constructor for teams that will be displayed:
   ActiveGameWidget(Team newTeam1, Team newTeam2) {
-    team1 = newTeam1;
-    team2 = newTeam2;
+    _team1 = newTeam1;
+    _team2 = newTeam2;
   }
 }
 
 class _ActiveGameWidgetState extends State<ActiveGameWidget> {
   @override
-  /*Team team1 = Team('Team 1', 6);
-  Team team2 = Team('Team 2', 6);
-  var _state = [team1, team2];
+  Team _team1 = Team('Team 1', 6);
+  Team _team2 = Team('Team 2', 6);
+
+  //was trying to set the state by setting the teams that are playing, got some errors
+  /*var _state = [team1, team2];
 
   makeNewGame(Team team1, Team team2, Playlist playlist) {
     algorithm(team1, team2, playlist);
@@ -36,50 +38,56 @@ class _ActiveGameWidgetState extends State<ActiveGameWidget> {
   }*/
 
   Widget build(BuildContext context) {
+    Team team1 = Team("Team 1", 6);
+    Team team2 = Team("Team 2", 3);
+    Player player = Player("Alex", "Wallen", "OH", 0, "path/path");
     return Stack(children: [
       //displaying the players on each team:
+      //Text('Team 1'),
       Container(
           height: 350,
+          width: 179,
           color: Colors.amber,
           child: GridView.count(
               padding: EdgeInsets.all(22),
               crossAxisSpacing: 10,
               mainAxisSpacing: 10,
-              crossAxisCount: 4,
+              crossAxisCount: 2,
               scrollDirection: Axis.vertical,
               children: [
-                ActivePlayerWidget(),
-                ActivePlayerWidget(),
-                ActivePlayerWidget(),
-                ActivePlayerWidget(),
-                ActivePlayerWidget(),
-                ActivePlayerWidget(),
-                ActivePlayerWidget(),
-                ActivePlayerWidget(),
-                ActivePlayerWidget(),
-                ActivePlayerWidget(),
-                ActivePlayerWidget(),
-                ActivePlayerWidget(),
-                ActivePlayerWidget(),
+                //makes the number of active player widgets that correspond to the team size:
+                for (int i = 0; i < (team1.teamSize); i++) ActivePlayerWidget()
               ])),
+      //Positioning the other container to the right so that the two don't overlap:
+      Positioned(
+        right: 0,
+        child: Container(
+            height: 350,
+            width: 179,
+            color: Colors.lightGreen,
+            child: GridView.count(
+                padding: EdgeInsets.all(22),
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+                crossAxisCount: 2,
+                scrollDirection: Axis.vertical,
+                children: [
+                  //makes the number of active player widgets that correspond to the team size:
+                  for (int i = 0; i < (team2.teamSize); i++)
+                    ActivePlayerWidget()
+                ])),
+      ),
 
       //divider won't show up
-      VerticalDivider(
-          color: Colors.black, thickness: 10, indent: 0, endIndent: 0),
+      /*VerticalDivider(
+          color: Colors.black, thickness: 10, indent: 0, endIndent: 0),*/
 
       //headings of "Team 1" and "Team 2"
-      Container(
-          height: 18,
-          color: Colors.lightBlue,
-          child: Row(children: [
-            Expanded(
-                child: Align(
-                    child: Text('       Team 1'),
-                    alignment: Alignment.topLeft)),
-            Expanded(
-                child: Align(
-                    child: Text('Team 2      '), alignment: Alignment.topRight))
-          ])),
+      Row(children: [
+        //it's only necessary to expand one of them to fill the available space
+        Expanded(child: Text('       Team 1')),
+        Text('Team 2        ')
+      ])
     ]);
   }
 }
