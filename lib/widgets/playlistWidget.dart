ddad3e592ab62485178e080fa141b90d1dae7c83
Nortus222 <Nortus222@gmail.com>
2020-12-17 17:17:47 -0800

    Updated playlist widget

--- lib/widgets/playlistWidget.dart
+++ lib/widgets/playlistWidget.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:kingzcourt/classes/Playlist.dart';
+import 'package:kingzcourt/classes/colors.dart';
+import 'package:kingzcourt/pages/landing.dart';
 import 'package:kingzcourt/widgets/bottomSheetWidget.dart';
 import '../classes/player.dart';
 import 'package:kingzcourt/database/databaseHelper.dart';
@@ -9,69 +12,76 @@ class PlaylistWidget extends StatefulWidget {
 }
 
 class _PlaylistWidgetState extends State<PlaylistWidget> {
-  final List<String> itemsTest =
-      List<String>.generate(20, (i) => "Player $i");
+  final List<String> itemsTest = List<String>.generate(20, (i) => "Player $i");
+  final Playlist playlist = LandingPage.playlist;
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: itemsTest.length,
-      itemBuilder: (context, index) {
-        return Card(
-          child: ListTile(
-            title: Row(
-              children: [
-                Expanded(child: Text('${itemsTest[index]}')),
-                Wrap(
+    if (playlist.isNotEmpty) {
+      return ListView.builder(
+        itemCount: itemsTest.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child: ListTile(
+              title: Row(
+                children: [
+                  Expanded(child: Text('${playlist.elementAt(index)}')),
+                  Wrap(
+                      direction: Axis.vertical,
+                      crossAxisAlignment: WrapCrossAlignment.center,
+                      children: [
+                        Container(
+                          height: 15,
+                        ),
+                        Text(
+                          //TODO
+                          '',
+                          style: TextStyle(fontSize: 12),
+                        ),
+                        Text(
+                          'pos',
+                          style:
+                              TextStyle(fontSize: 16, color: Colors.grey[700]),
+                        )
+                      ]),
+                  Container(
+                    width: 15,
+                  ),
+                  Wrap(
                     direction: Axis.vertical,
                     crossAxisAlignment: WrapCrossAlignment.center,
                     children: [
                       Container(
                         height: 15,
                       ),
+                      Text('0', style: TextStyle(fontSize: 12)),
                       Text(
-                        //TODO
-                        'OH',
-                        style: TextStyle(fontSize: 12),
-                      ),
-                      Text(
-                        'pos',
+                        'wait',
                         style: TextStyle(fontSize: 16, color: Colors.grey[700]),
-                      )
-                    ]),
-                Container(
-                  width: 15,
-                ),
-                Wrap(
-                  direction: Axis.vertical,
-                  crossAxisAlignment: WrapCrossAlignment.center,
-                  children: [
-                    Container(
-                      height: 15,
-                    ),
-                    Text('0', style: TextStyle(fontSize: 12)),
-                    Text(
-                      'wait',
-                      style: TextStyle(fontSize: 16, color: Colors.grey[700]),
-                    ),
-                  ],
-                )
-              ],
-            )
-            //Text('${itemsTest[index]}')
-            ,
-            leading: Icon(Icons.account_circle_outlined),
-            trailing: IconButton(
-                icon: Icon(Icons.keyboard_control),
-                onPressed: () {
-                  showModalBottomSheet(
-                    context: context,
-                    builder: (context) => BottomSheetWidget(),
-                  );
-                }),
-          ),
-        );
-      },
-    );
+                      ),
+                    ],
+                  )
+                ],
+              ),
+              leading: Icon(Icons.account_circle_outlined),
+              trailing: IconButton(
+                  icon: Icon(Icons.keyboard_control),
+                  onPressed: () {
+                    showModalBottomSheet(
+                      context: context,
+                      builder: (context) => BottomSheetWidget(),
+                    );
+                  }),
+            ),
+          );
+        },
+      );
+    } else {
+      return Container(
+        alignment: Alignment.center,
+        child: Text('Playlist is empty. Try adding players.',
+            style: TextStyle(fontSize: 20, color: AppColors.primaryAccentDark)),
+      );
+    }
   }
 }
