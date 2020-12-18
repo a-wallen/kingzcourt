e0cbfd745011f8257f9c6189e4cf487ed588c9d7
Nortus222 <Nortus222@gmail.com>
2020-12-17 17:16:37 -0800

    Updated landing page

--- lib/pages/landing.dart
+++ lib/pages/landing.dart
@@ -9,10 +9,14 @@ import 'package:kingzcourt/widgets/playericon.dart';
 import 'package:kingzcourt/classes/player.dart';
 import 'package:kingzcourt/classes/team_ihor.dart';
 import 'package:kingzcourt/widgets/playlistWidget.dart';
+import 'package:kingzcourt/classes/Playlist.dart';
 
 class LandingPage extends StatefulWidget {
   LandingPage({Key key, this.title}) : super(key: key);
   final String title;
+  static Team team1L = Team('Team1', 6);
+  static Team team2L = Team('team2', 6);
+  static Playlist playlist = Playlist();
   @override
   _LandingPageState createState() => _LandingPageState();
 }
@@ -20,28 +24,74 @@ class LandingPage extends StatefulWidget {
 class _LandingPageState extends State<LandingPage> {
   int currentIndex = 0;
 
+  refresh() {
+    print('refresh');
+    setState(() {});
+  }
+
+  Map<String, int> dropValues = {
+    'Two': 2,
+    'Three': 3,
+    'Four': 4,
+    'Five': 5,
+    'Six': 6
+  };
+
+  String dropDownValue = 'Six';
+
   @override
   Widget build(BuildContext context) {
-    Player player = Player("Alex", "Wallen", "OH", 0, "path/path");
     return Scaffold(
         appBar: AppBar(
           textTheme: Theme.of(context).textTheme,
           title: Text(widget.title),
         ),
-        body: _buildBody(),
+        body: Column(
+          children: [
+            Container(
+                height: 35,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text('Team1'),
+                    Wrap(
+                        crossAxisAlignment: WrapCrossAlignment.center,
+                        children: [
+                          Text('Team size: '),
+                          DropdownButton<String>(
+                              dropdownColor: Colors.white,
+                              value: dropDownValue,
+                              items: <String>[
+                                'Two',
+                                'Three',
+                                'Four',
+                                'Five',
+                                'Six'
+                              ].map<DropdownMenuItem<String>>((value) {
+                                return DropdownMenuItem(
+                                    value: value, child: Text(value));
+                              }).toList(),
+                              onChanged: (String newValue) {
+                                setState(() {
+                                  dropDownValue = newValue;
+                                  LandingPage.team1L
+                                      .updateTeamSize(dropValues[newValue]);
+                                  LandingPage.team2L
+                                      .updateTeamSize(dropValues[newValue]);
+                                });
+                              }),
+                        ]),
+                    Text('Team2'),
+                  ],
+                )),
+            Divider(),
+            ActiveGameWidget(LandingPage.team1L, LandingPage.team2L),
+            AddPlayerButton(callback: refresh()),
+            Expanded(child: PlaylistWidget()),
+          ],
+        ),
         drawer: KingzDrawer(),
         floatingActionButton: RefreshGameButton());
   }
-}
-
-Widget _buildBody() {
-  return Column(
-    children: [
-      ActiveGameWidget(Team('Team 1', 6), Team('Team 2', 6)),
-      Divider(),
-      Text('Add Player Buton goes here'),
-      Divider(),
-      Expanded(child: PlaylistWidget()),
-    ],
-  );
 }
