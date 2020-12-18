1b4064b1a4445c3c6082f8bb74fe4728e9189233
Nortus222 <Nortus222@gmail.com>
2020-12-17 17:16:55 -0800

    Added addPlayer button

--- lib/widgets/add_player_button.dart
+++ lib/widgets/add_player_button.dart
@@ -1,8 +1,20 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:kingzcourt/classes/Playlist.dart';
+import 'package:kingzcourt/classes/colors.dart';
 import 'package:kingzcourt/classes/player.dart';
+import 'package:kingzcourt/pages/playerlibrary.dart';
 
 class AddPlayerButton extends StatefulWidget {
+  final Function() callback;
+
+  FutureOr refresh() {
+    callback;
+  }
+
+  AddPlayerButton({Key key, @required this.callback}) : super(key: key);
+
   @override
   _AddPlayerButton createState() => _AddPlayerButton();
 }
@@ -10,16 +22,18 @@ class AddPlayerButton extends StatefulWidget {
 class _AddPlayerButton extends State<AddPlayerButton> {
   Playlist playlist = Playlist();
   Widget build(BuildContext context) {
-    return Positioned(
-      bottom: 20,
-      left: 100,
-      right: 100,
-      child: RaisedButton(
-          onPressed: () {
-            playlist.add(Player("Firstname", "Lastname", "OH", 0, "path/path"));
-            Text('Player added');
-          },
-          child: Text('Add Player:')),
+    return RaisedButton(
+      onPressed: () {
+        Navigator.push(context,
+                MaterialPageRoute(builder: (context) => PlayerLibraryPage()))
+            .then(widget.refresh());
+        print('testButton');
+      },
+      color: AppColors.accentColor,
+      child: Text(
+        'Add player',
+        style: TextStyle(fontSize: 16, color: AppColors.primaryAccent),
+      ),
     );
   }
 }
